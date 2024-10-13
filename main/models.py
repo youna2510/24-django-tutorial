f70ade52c6b7e3ab30984501be08f28624a2c47f
tonynamy <tonynamy@apperz.co.kr>
2024-10-14 00:45:28 +0900

    ruff check + ruff format

--- main/models.py
+++ main/models.py
@@ -2,6 +2,7 @@ from django.db import models
 
 # Create your models here.
 
+
 class Study(models.Model):
     name = models.CharField(max_length=500, verbose_name="스터디 이름")
     description = models.CharField(max_length=2000, verbose_name="스터디 설명")
