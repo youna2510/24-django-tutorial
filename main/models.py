befbad7c33ab948e4c89a52480ab2ecf8b9bb4db
tonynamy <tonynamy@apperz.co.kr>
2024-10-14 00:02:10 +0900

    Study 모델 추가

--- main/models.py
+++ main/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+
+class Study(models.Model):
+    name = models.CharField(max_length=500, verbose_name="스터디 이름")
+    description = models.CharField(max_length=2000, verbose_name="스터디 설명")
+
+    class Meta:
+        db_table = "study"
+        verbose_name = "스터디"
