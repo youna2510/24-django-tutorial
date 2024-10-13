be29d0b5fdbdd584073a54ab15159a903013c440
tonynamy <tonynamy@apperz.co.kr>
2024-10-14 02:59:56 +0900

    커스텀 유저 모델 정의

--- main/models.py
+++ main/models.py
@@ -1,6 +1,15 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
 # Create your models here.
+
+
+class User(AbstractUser):
+    student_number = models.CharField(max_length=8, null=True, verbose_name="학번")
+
+    class Meta:
+        db_table = "user"
+        verbose_name = "유저"
 
 
 class Study(models.Model):
