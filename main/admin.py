47539a58776c158a968a7f905942909243278e1a
tonynamy <tonynamy@apperz.co.kr>
2024-10-14 03:10:56 +0900

    어드민 추가

--- main/admin.py
+++ main/admin.py
@@ -1,1 +1,11 @@
 # Register your models here.
+
+from django.contrib import admin
+from main.models import User
+
+
+class UserAdmin(admin.ModelAdmin):
+    pass
+
+
+admin.site.register(User, UserAdmin)
