07abdf3ca4356722b2b1825dd33ce8c0d867ed27
tonynamy <tonynamy@apperz.co.kr>
2024-10-14 00:45:06 +0900

    rest framework 설정

--- django_tutorial/settings.py
+++ django_tutorial/settings.py
@@ -123,3 +123,13 @@ STATIC_URL = "static/"
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+REST_FRAMEWORK = {
+    "DEFAULT_AUTHENTICATION_CLASSES": [
+        "rest_framework.authentication.SessionAuthentication",
+        "rest_framework.authentication.BasicAuthentication",
+    ],
+    "DEFAULT_PERMISSION_CLASSES": [
+        "rest_framework.permissions.AllowAny",
+    ],
+}
