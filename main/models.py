cf36da33286f4ed213fbcea84daa580bdf05b29a
tonynamy <tonynamy@apperz.co.kr>
2024-10-14 04:08:24 +0900

    스터디 생성자 추가

--- main/models.py
+++ main/models.py
@@ -15,6 +15,7 @@ class User(AbstractUser):
 class Study(models.Model):
     name = models.CharField(max_length=500, verbose_name="스터디 이름")
     description = models.CharField(max_length=2000, verbose_name="스터디 설명")
+    created_by = models.ForeignKey(to=User, on_delete=models.PROTECT)
 
     class Meta:
         db_table = "study"
