8ca9b96587514c8aa989f948f09f1b811f1097ac
Youna <youna2510@naver.com>
2024-11-07 22:49:50 +0900

    Complete assignment-1 code changes

--- main/views.py
+++ main/views.py
@@ -22,6 +22,23 @@ class CalculatorAPIView(GenericAPIView):
 
         result: Decimal = None
         ## assignment1: 이곳에 과제를 작성해주세요
+        input_a = serializer.validated_data['input_a']
+        input_b = serializer.validated_data['input_b']
+
+        operator = serializer.validated_data['operator']
+
+        if operator == '+':
+            result = Decimal(input_a) + Decimal(input_b)
+        elif operator == '-':
+            result = Decimal(input_a) - Decimal(input_b)
+        elif operator == '*':
+            result = Decimal(input_a) * Decimal(input_b)
+        elif operator == '/':
+            if input_b == 0:
+                return Response({"error": "Division by zero is not allowed"})
+            result = Decimal(input_a) / Decimal(input_b)
+        else:
+            return Response({"error": "Invalid operator"})
         ## end assignment1
 
         # serialization
