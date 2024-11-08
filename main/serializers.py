e5ba6e39755cd8942a2233299dafc23197f776a5
Youna <youna2510@naver.com>
2024-11-08 16:19:25 +0900

    수정 완료

--- main/serializers.py
+++ main/serializers.py
@@ -1,10 +1,10 @@
 from rest_framework import serializers
+from main.models import Student
 
 
 class StudentSerializer(serializers.ModelSerializer):
-    """
-    학생 모델에 대한 Serializer
-    """
+    class Meta:
+        model = Student
+        fields = ["id", "name", "student_number", "primary_major"]
 
-    ### assignment2: 이곳에 과제를 작성해주세요
-    ### end assignment2
+
--- main/views.py
+++ main/views.py
@@ -9,25 +9,31 @@ from rest_framework.mixins import (
     DestroyModelMixin,
 )
 
+from main.models import Student
+from main.serializers import StudentSerializer
 
 class StudentListAPIView(ListModelMixin, CreateModelMixin, GenericAPIView):
-    """
-    GET: 학생 목록 조회
-    POST: 학생 추가
-    """
+    queryset = Student.objects.all()
+    serializer_class = StudentSerializer
 
-    ### assignment2: 이곳에 과제를 작성해주세요
-    ### end assignment2
+    def get(self, request):
+        return self.list(request)
+    def post(self, request):
+        return self.create(request)
+
 
 
 class StudentAPIView(
     RetrieveModelMixin, UpdateModelMixin, DestroyModelMixin, GenericAPIView
 ):
-    """
-    GET: 학생 조회
-    PATCH: 학생 수정
-    DELETE: 학생 삭제
-    """
+    queryset = Student.objects.all()
+    serializer_class = StudentSerializer
 
-    ### assignment2: 이곳에 과제를 작성해주세요
-    ### end assignment2
+    def get(self, request, **kwargs):
+        return self.retrieve(request, **kwargs)
+
+    def patch(self, request, *args, **kwargs):
+        return self.partial_update(request, **kwargs)
+
+    def delete(self, request, **kwargs):
+        return self.destroy(request, **kwargs)
